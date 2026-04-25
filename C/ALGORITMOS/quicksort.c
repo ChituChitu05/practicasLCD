a17bd445428eb45c53b739a62d2d6f066a8409e8
Just Chitu <cchitu05@gmail.com>
2026-04-25 16:09:45 -0600

    add:python practices

--- C/ALGORITMOS/quicksort.c
+++ C/ALGORITMOS/quicksort.c
@@ -15,21 +15,81 @@ Ahora bien, el objetivo aquí es reorganizar la lista de tal manera que todos lo
 3. Dividir los subconjuntos
 Una vez que hayamos particionado el arreglo, podemos dividir este problema en dos subproblemas. Primero, ordenamos el segmento del arreglo a la izquierda del pivote, y luego ordenamos el segmento del arreglo a la derecha del pivote.
 */
-
-
 #include <stdio.h>
-int swap(int *arr,int *a, int *b){
-    int temp = *a;
+#include <stdlib.h>
+#include <time.h>
+/*
+Recibe: 
+-La direccion del arreglo. 
+-La direccion del primer primer elemento y el utimo del arreglo/subarreglo a ordenar.
+*/
+
+int swap(int *a,int *b){
+    int temp;
+    temp = *a;
     *a = *b;
     *b = temp;
     return 0;
 }
-int partition (int *arr,int *start,int *end){
-    
+
+int* particion(int *start,int *end){
+    int *i;
+    int *pivote = end;
+    int *peque = start - 1;
+
+    for(i = start; i < end; i++){
+        if(*i < *pivote){
+            peque++;
+            swap(peque,i);
+        }
+    }
+    swap(peque + 1, end);
+    return peque + 1;
 }
-int main(){
-    int arr[10] = {0,1,2,3,4,5,6,7,8,9};
 
+int quickSort(int *start,int *end){
+    if(start >= end) return 0;
 
+    int *pivote = particion(start,end);
+    quickSort(start,pivote - 1);
+    quickSort(pivote + 1,end);
     return 0;
 }
+
+//FUNCIONES AUXILIARES
+int rellenarArreglo(int *ptrInicio,int *ptrFin){
+    srand(time(NULL));
+    for (; ptrInicio < ptrFin; ptrInicio++) {
+        *ptrInicio = rand() % 100;  
+    }
+    return 0;
+}
+int imprimirArreglo(int *ptrInicio,int *ptrFin){
+        printf("[");
+    for (; ptrInicio < ptrFin; ptrInicio++) {
+        printf(" %d ",*ptrInicio); 
+    }
+    printf("]");
+    return 0;
+}
+
+
+
+int main(){
+    /*Inicializar un arreglo de n numeros de enteros aleatoreos. */
+    int arr[10];
+    int *ptrStart, *ptrEnd;
+    ptrStart = arr;
+    ptrEnd = &arr[10];
+    rellenarArreglo(ptrStart,ptrEnd);
+    ptrStart = arr;
+    printf("Arrgeglo creado: \n ");
+    imprimirArreglo(ptrStart,ptrEnd);
+    quickSort(ptrStart,ptrEnd);
+    ptrStart = arr;
+    printf("\nArreglo ordenado:\n ");
+    imprimirArreglo(ptrStart,ptrEnd);
+    
+    return 0;
+}
+
