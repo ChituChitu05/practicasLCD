79d79d7e6510b816e212ef6613e1950a46773562
Just Chitu <cchitu05@gmail.com>
2026-04-25 23:06:25 -0600

    fix:practicas

---
+++ C/ESTRUCTURAS DE DATOS/ESTRUCTURAS DE DATOS/cadenaToListv1.c
@@ -0,0 +1,61 @@
+//pasar cadena a lista con cabecera
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Nodo{
+    char caracter;
+    struct Nodo *ptrSig,*ptrAnt;
+};
+
+struct Nodo *crearNodo(char *caracter){
+    struct Nodo *nodoNuevo;
+    nodoNuevo = (struct Nodo *) malloc(sizeof(struct Nodo));
+    strcpy(&nodoNuevo -> caracter,caracter);
+    nodoNuevo -> ptrSig = NULL;
+    nodoNuevo -> ptrAnt = NULL;
+    return nodoNuevo;
+}
+
+int meterDato(struct Nodo *ptrRef,char *caracter){
+    struct Nodo *ptrNuevo;
+    ptrNuevo = crearNodo(caracter);
+    if(ptrRef ->ptrSig == NULL){
+        ptrRef -> ptrSig = ptrNuevo;
+        ptrNuevo -> ptrAnt = ptrRef;
+    }else{
+        ptrNuevo -> ptrSig = ptrRef -> ptrSig;
+        ptrNuevo -> ptrAnt = ptrRef;
+        ptrRef -> ptrSig -> ptrAnt = ptrNuevo;
+        ptrRef -> ptrSig = ptrNuevo;
+    }
+    return 0;
+}
+
+int imprimir(struct Nodo *ptrRef){
+    struct Nodo *ptrRec;
+    int i;
+    i=0;
+    ptrRec = ptrRef ->ptrSig;
+    while(ptrRec != NULL){
+        i++;
+        printf("Caracter %d : %c\n",i, ptrRec -> caracter);
+        ptrRec = ptrRec -> ptrSig;
+    }
+    return 0;
+}
+int main(){
+    char cadena[30],*ptrC;
+    struct Nodo *ptrRef;
+    ptrRef = crearNodo("");
+    strcpy(cadena,"uwu");
+    ptrC = cadena;
+
+    while(*ptrC != '\0'){
+        meterDato(ptrRef,ptrC);
+        ++ptrC;
+    }
+    imprimir(ptrRef);
+
+    return 0;
+}
