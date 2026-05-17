6bb787afe2cf999475b64cf75ea0b2724a74ecb1
Just Chitu <cchitu05@gmail.com>
2026-05-16 18:29:33 -0600

    add:practicas

--- C/ESTRUCTURAS DE DATOS/ESTRUCTURAS DE DATOS/30-mcd.c
+++ C/ESTRUCTURAS DE DATOS/ESTRUCTURAS DE DATOS/30-mcd.c
@@ -1,11 +1,11 @@
 //mcd
 #include <stdio.h>
-
 int mcd(int a, int b){
-    if(a == 0 && b==0) return 0;
-
-    if(a>=b && a%b != 0) return a-mcd(a-b,a);
-    if(b>=a && b%a != 0) return b-mcd(a,b-a);
+    if (b == 0) return a;
+    if (a == 0) return b;
+    if(a == 0 && b == 0) return 0;
+    if(a > b) return mcd(a-b ,b);
+    return mcd(a,b-a);
 }
 
 int main(){
